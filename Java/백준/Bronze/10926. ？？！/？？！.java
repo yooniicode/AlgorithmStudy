ed0e77659da8e438b1fbd755ab4ad29dd3a36dbf
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-03-20 12:42:00 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String id = s.nextLine();
+        System.out.println(id + "??!");
+    }
+}

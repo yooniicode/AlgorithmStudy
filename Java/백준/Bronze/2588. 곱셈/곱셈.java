01b977c5c26c874c4c61503493a15e81961b1217
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-03-20 13:13:13 +0900

    [Bronze III] Title: 곱셈, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a*(b%10));
+		System.out.println(a*((b%100)/10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
+		
+		s.close();
+	}
+	
+
+}

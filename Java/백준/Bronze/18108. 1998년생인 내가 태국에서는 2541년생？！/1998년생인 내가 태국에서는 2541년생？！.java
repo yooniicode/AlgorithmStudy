12a2c50ca0dd3b0ce4fea1717d3941fbc1b2a86d
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-03-20 12:51:57 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ Java/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Integer b = 2541-1998;
+		
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		Integer a = s.nextInt();
+		
+		System.out.println(a-b);
+	}
+
+}

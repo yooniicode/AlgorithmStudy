7ed392b9cc7175b957380cd91b92a7ac5b4dd137
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-03-20 12:48:03 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		Integer a = s.nextInt();
+		Integer b = s.nextInt();
+		
+		System.out.println(a+b);
+	}
+
+}
